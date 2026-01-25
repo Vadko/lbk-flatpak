1d0af4b37c81f6fcee2970568986ec80b39c2de7
Vadko <vpoglid@gmail.com>
2026-01-25 20:01:36 +0200

    improve flatpack perf

--- lbk-launcher.sh
+++ lbk-launcher.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-exec /app/lbk-launcher/lbk-launcher --no-sandbox "$@"
+
+exec /app/lbk-launcher/lbk-launcher \
+    --no-sandbox \
+    --enable-gpu-rasterization \
+    --enable-zero-copy \
+    --ignore-gpu-blocklist \
+    --enable-features=VaapiVideoDecoder,CanvasOopRasterization,UseOzonePlatform \
+    --ozone-platform-hint=auto \
+    "$@"
